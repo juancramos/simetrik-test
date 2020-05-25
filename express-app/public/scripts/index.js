7a816b377beae3132c2841cb4999f86bdf710f92
Juan Carlos Hernandez Ramos <jhernandez@addi.com>
2020-05-25 02:05:26 -0500

    sync and nice list

--- express-app/public/scripts/index.js
+++ express-app/public/scripts/index.js
@@ -1,9 +1,26 @@
 const ul = document.querySelector('ul');
 
-const setColumn = (column) => {
-    setTimeout(() => {
-        ul.innerHTML += `<li>${column}<li>`
-    }, Math.random() * 5000);
+const addChild = (column) => {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(column));
+    ul.appendChild(li);
 }
 
-setColumn('hola');
+const setColumn = (column) => {
+    const randomDelay = Math.floor(Math.random() * 5000) + 1;
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(addChild(column));
+        }, randomDelay);
+    });
+}
+
+const columns = ['Columna SKT_ID', 'Columna Amount', 'Columna Getway', 'Columna Date', 'Columna Issuer', 'Columna User_id'];
+
+const showColumns = async () => {
+    for (let col of columns) {
+        await setColumn(col);
+    }
+}
+
+showColumns();
