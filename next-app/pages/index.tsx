14cc4e8e452da156f6d6b615d9c2770cb706a4a0
Juan Carlos Hernandez Ramos <jhernandez@addi.com>
2020-05-25 15:11:58 -0500

    start styling

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { GlobalState } from '@store/index';
 import { Layout } from '@components/layout/Layout';
+import { Row, Column } from '@components/grid';
+import { Gap } from '@components/gap/Gap';
 
 const Index = () => {
   const { columnNames } = useSelector((state: GlobalState) => state.dndListReducer);
@@ -9,11 +11,20 @@ const Index = () => {
   return (
 
     <Layout helmetTags={<title>List</title>}>
-      <>
-        {columnNames.map(colName => (
-          <p key={colName}>{colName}</p>
-        ))}
-      </>
+      <Gap size={40} />
+      <Row>
+        <Column size={1}>
+        </Column>
+        <Column size={2}>
+          {columnNames.map(colName => (
+            <p key={colName}>{colName}</p>
+          ))}
+        </Column>
+        <Column size={2}>
+        </Column>
+        <Column size={1}>
+        </Column>
+      </Row>
     </Layout>
   );
 };
